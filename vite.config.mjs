892e144dc3943455446299cf95ca92548ecfc51d
amgst <amgst99@gmail.com>
2026-09-02 11:54:13 +0500

    d

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,11 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import { realpathSync } from "node:fs";
+import { fileURLToPath } from "node:url";
+
+const root = realpathSync(fileURLToPath(new URL(".", import.meta.url)));
 
 export default defineConfig({
+  root,
   plugins: [reactRouter()],
 });
