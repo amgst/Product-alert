3ee6478a52c789df27571a5a619016f5e21c1652
amgst <amgst99@gmail.com>
2026-09-03 19:32:50 +0500

    Include build/server files in vercel.json includeFiles for api/index.js serverless handler

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import { createRequestHandler } from "@react-router/node";
+import * as build from "../build/server/index.js";
 
 export default createRequestHandler({
-  build: () => import("../build/server/index.js"),
+  build,
 });
